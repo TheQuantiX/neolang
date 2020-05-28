9764eba1bbb4621934fc37239fc02dafe624e81f
TheQuantiX <thequantix12@gmail.com>
2020-05-28 12:28:13 +0300

    Refactored!!!

--- NeoLangCPP/Tokenizer.cpp
+++ NeoLangCPP/Tokenizer.cpp
@@ -1,41 +1,7 @@
 #include "Tokenizer.h"
-#include <regex>
 #include <algorithm>
 
 Tokenizer::Tokenizer() {}
-
-bool Tokenizer::isInt(std::string exp) {
-	return std::regex_match(exp, std::regex("^(-)?[0-9]+$"));
-}
-
-bool Tokenizer::isNum(std::string exp) {
-	return std::regex_match(exp, std::regex("^(-)?[0-9]+(\.[0-9]*)?$"));
-}
-
-bool Tokenizer::isBool(std::string exp) {
-	return std::regex_match(exp, std::regex("^(true|false)$"));
-}
-
-bool Tokenizer::isString(std::string exp) {
-	return std::regex_match(exp, std::regex("^(\".*\"|\'.*\')$"));
-}
-
-bool Tokenizer::isOP(std::string exp) {
-	std::vector<std::string> ops{ "+", "-", "*", "%", "**", "/", "++", "--", "+=", "-=", "*=", "/=", "**=", "%=", "and", "or", "xor", "not", "&", "|", "^", "~", "==", "=", ">", "<", ">=", "<=" };
-	return std::find(ops.begin(), ops.end(), exp) != ops.end();
-}
-
-bool Tokenizer::isVar(std::string exp) {
-	return std::regex_match(exp, std::regex("^[a-zA-Z_][a-zA-Z_0-9]*$"));
-}
-
-bool Tokenizer::isKeyword(std::string exp) {
-	return std::regex_match(exp, std::regex("^for|while|if|else|switch|case|default|break|continue|return|int|string|float|bool|function|include$"));
-}
-
-bool Tokenizer::isWS(std::string exp) {
-	return std::regex_match(exp, std::regex("^[ \t\n]$"));
-}
 
 std::vector<std::string> Tokenizer::StrToken(std::string exp) {
 	Tokenizer::isTheString IS = NONE;
